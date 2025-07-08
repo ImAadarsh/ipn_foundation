e019249cc058a5116a39d43dcfcb835d1028e577
ImAadarsh <aadarshkavita@gmail.com>
2025-07-08 23:35:56 +0530

    Footer also

---
+++ include/footer.php
@@ -0,0 +1,142 @@
+<footer>
+               <!-- tp-footer area start -->
+               <div class="tp-footer-area bg-position pt-100 pb-40" data-background="assets/img/footer/bg.jpg">
+                  <div class="tp-footer-3-top pb-15">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-lg-7 mb-30">
+                              <div class="tp-footer-3-top-content">
+                                 <div class="tp-benifits-form-content d-flex align-items-center">
+                                    <span class="tp-icon d-inline-block tp-bg-common-white tp-round-24 mr-15">
+                                       <svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M10.5474 24C11.9861 24 13.225 23.131 13.7686 21.8906H7.32617C7.86978 23.131 9.10873 24 10.5474 24Z" fill="#1F3130" />
+                                          <path d="M17.8126 11.6185V10.0781C17.8126 6.80522 15.637 4.0312 12.6563 3.12516V2.10938C12.6563 0.946266 11.71 0 10.5469 0C9.38382 0 8.43755 0.946266 8.43755 2.10938V3.12516C5.45677 4.0312 3.2813 6.80517 3.2813 10.0781V11.6185C3.2813 14.4935 2.18546 17.2195 0.195664 19.2946C0.00066416 19.4979 -0.0541328 19.798 0.0563985 20.0571C0.16693 20.3162 0.421461 20.4844 0.70318 20.4844H20.3907C20.6724 20.4844 20.9269 20.3162 21.0374 20.0571C21.1479 19.798 21.0931 19.4979 20.8982 19.2946C18.9084 17.2195 17.8126 14.4934 17.8126 11.6185ZM11.2501 2.84663C11.0186 2.82431 10.7841 2.8125 10.5469 2.8125C10.3097 2.8125 10.0752 2.82431 9.8438 2.84663V2.10938C9.8438 1.72167 10.1592 1.40625 10.5469 1.40625C10.9346 1.40625 11.2501 1.72167 11.2501 2.10938V2.84663Z" fill="#1F3130" />
+                                       </svg>
+                                    </span>   
+                                    <div class="tp-footer-3-title">
+                                       <h3 class="fw-500 fs-30 fs-xs-22 tp-text-common-white mb-5 text-anim">Sign up to our newsletter</h3>
+                                       <p class="mb-0 tp-text-grey-3">Subscribe to our newsletter for more consulting update!</p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="col-lg-5">
+                              <div class="tp-footer-3-form ml-80 mb-30">
+                                 <form action="#" class="p-relative">
+                                    <input class="tp-footer-input" type="text" placeholder="Your email address">
+                                    <button type="submit" class="footer-3-btn tp-round-2 tp-text-common-white tp-bg-theme-primary d-inline-block">
+                                       <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                          <path d="M1 7H13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                          <path d="M7 1L13 7L7 13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                       </svg>
+                                    </button>
+                                 </form>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="pt-80">
+                     <div class="container">
+                        <div class="row">
+                           <div class="col-lg-4 col-md-6 col-sm-6">
+                              <div class="tp-footer-widget tp-footer-3-widget mb-40 mr-10">
+                                 <a class="mb-30 d-inline-block" href="index.php"><img data-width="168" src="assets/img/logo/logo-white.png" alt=""></a>
+                                 <p class="tp-text-grey-3 mb-30">A startup vision summary should be a concise
+                                    statement that captures the essence of your
+                                    company’s long-term goal</p>
+                                 <div class="tp-footer-bottom-social mb-20">
+                                    <ul class="d-flex align-items-center">
+                                       <li>
+                                          <a href="#">
+                                             <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M2.403 9.03129C2.34293 9.03129 1.02128 9.03129 0.420526 9.03129C0.100125 9.03129 0 8.91114 0 8.61076C0 7.80976 0 6.98874 0 6.18773C0 5.86733 0.12015 5.76721 0.420526 5.76721H2.403C2.403 5.70713 2.403 4.54568 2.403 4.00501C2.403 3.20401 2.54318 2.44305 2.94368 1.74218C3.3642 1.02128 3.96496 0.540676 4.72591 0.260325C5.22653 0.0801 5.72716 0 6.26783 0H8.23029C8.51064 0 8.63079 0.12015 8.63079 0.400501V2.68335C8.63079 2.9637 8.51064 3.08385 8.23029 3.08385C7.68961 3.08385 7.14894 3.08385 6.60826 3.10388C6.06758 3.10388 5.78723 3.36421 5.78723 3.92491C5.76721 4.52566 5.78723 5.10638 5.78723 5.72716H8.11014C8.43054 5.72716 8.55069 5.84731 8.55069 6.16771V8.59074C8.55069 8.91114 8.45056 9.01126 8.11014 9.01126C7.38924 9.01126 5.84731 9.01126 5.78723 9.01126V15.5394C5.78723 15.8798 5.68711 16 5.32666 16C4.48561 16 3.66458 16 2.82353 16C2.52315 16 2.403 15.8799 2.403 15.5795C2.403 13.4768 2.403 9.09136 2.403 9.03129Z" fill="currentColor" />
+                                             </svg>
+                                          </a>
+                                       </li>
+                                       <li>
+                                          <a href="#">
+                                             <svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M0.743794 5.331C0.743794 5.331 2.28009 4.12018 2.79238 4.72531C3.3041 5.331 5.25871 12.64 5.91139 13.9878C6.48098 15.1705 8.05166 16.7332 9.77419 15.6169C11.4956 14.5006 17.2213 9.61269 18.2459 3.84055C19.2699 -1.92988 11.3563 -0.721354 10.518 4.30642C12.613 3.04862 13.7315 4.81699 12.6594 6.82031C11.589 8.82133 10.6108 10.1261 10.0997 10.1261C9.58853 10.1261 9.19543 8.78695 8.60979 6.44669C8.00524 4.02735 8.00811 -0.330546 5.49193 0.163406C3.11843 0.62928 0 4.35341 0 4.35341L0.743794 5.331Z" fill="currentColor" />
+                                             </svg>
+                                          </a>
+                                       </li>
+                                       <li>
+                                          <a href="#">
+                                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M9.60319 6.77491L15.4315 0H14.0504L8.98966 5.88256L4.94769 0H0.285767L6.39801 8.89547L0.285767 16H1.66696L7.01119 9.78782L11.2798 16H15.9417L9.60285 6.77491H9.60319ZM7.71145 8.97384L7.09216 8.08805L2.16463 1.03974H4.28606L8.26263 6.72795L8.88193 7.61374L14.051 15.0075H11.9296L7.71145 8.97418V8.97384Z" fill="currentColor" />
+                                             </svg>
+                                          </a>
+                                       </li>
+                                       <li>
+                                          <a href="#">
+                                             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M13.1428 1H5.14282C2.93368 1 1.14282 2.79086 1.14282 5V13C1.14282 15.2091 2.93368 17 5.14282 17H13.1428C15.352 17 17.1428 15.2091 17.1428 13V5C17.1428 2.79086 15.352 1 13.1428 1Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M12.3431 8.49624C12.4418 9.16204 12.3281 9.84201 12.0181 10.4395C11.7081 11.0369 11.2176 11.5214 10.6164 11.824C10.0151 12.1266 9.33381 12.2319 8.66928 12.125C8.00475 12.0181 7.39086 11.7043 6.91492 11.2284C6.43899 10.7524 6.12524 10.1385 6.01831 9.47401C5.91137 8.80948 6.0167 8.12816 6.31931 7.52694C6.62192 6.92572 7.1064 6.43523 7.70384 6.12523C8.30128 5.81523 8.98126 5.70151 9.64705 5.80024C10.3262 5.90095 10.9549 6.21741 11.4404 6.70289C11.9259 7.18836 12.2423 7.8171 12.3431 8.49624Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                                <path d="M13.5424 4.60059H13.5504" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                             </svg>
+                                          </a>
+                                       </li>
+                                    </ul>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="col-lg-3 col-md-6 col-sm-6">
+                              <div class="tp-footer-widget ml-70 mb-40">
+                                 <h5 class="fw-500 fs-24 tp-text-common-white mb-25">Useful links</h5>
+                                 <div class="tp-footer-menu">
+                                    <ul>
+                                       <li><a href="about.php">About us</a></li>
+                                       <li><a href="#">Our consultants</a></li>
+                                       <li><a href="service.php">Our Services</a></li>
+                                       <li><a href="#">Case studies</a></li>
+                                       <li><a href="contact.php">Contact us</a></li>
+                                    </ul>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="col-lg-3 col-md-6 col-sm-6">
+                              <div class="tp-footer-widget mb-40">
+                                 <h5 class="fw-500 fs-24 tp-text-common-white mb-25">Our services</h5>
+                                 <div class="tp-footer-menu">
+                                    <ul>
+                                       <li><a href="#">Search engine optimization</a></li>
+                                       <li><a href="#">Online business consulting</a></li>
+                                       <li><a href="#">Portfolio management</a></li>
+                                       <li><a href="#">Manage IT services</a></li>
+                                       <li><a href="#">Finance consulting</a></li>
+                                    </ul>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="col-lg-2 col-md-6 col-sm-6">
+                              <div class="tp-footer-widget tp-footer-meinus mb-40">
+                                 <h5 class="fw-500 fs-24 tp-text-common-white mb-25">Legality</h5>
+                                 <div class="tp-footer-menu">
+                                    <ul>
+                                       <li><a href="#">Privacy policy</a></li>
+                                       <li><a href="#">Terms and condition</a></li>
+                                       <li><a href="#">Cookie policy</a></li>
+                                    </ul>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="tp-footer-bottom pt-35">
+                     <div class="container">
+                        <div class="tp-footer-3-bottom">
+                           <div class="row">
+                              <div class="col-12">
+                                 <div class="tp-footer-bottom-copyright text-center">
+                                    <p class="tp-text-border-3 mb-0 lh-22">© <span id="year"></span> <a href="#" class="tp-text-common-white">IPN Foundation</a>. All Rights Reserved. Developed by <a href="https://endeavourdigital.in" class="tp-text-common-white">Endeavour Digital</a></p>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <!-- tp-footer area end -->
+            </footer>
